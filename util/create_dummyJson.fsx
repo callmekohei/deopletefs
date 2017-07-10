95f42f1120cd52122f0625d286b85cc80b73da19
callmekohei <callmekohei@gmail.com>
2017-07-10 09:04:31 +0900

    code refactor

--- util/create_dummyJson.fsx
+++ util/create_dummyJson.fsx
@@ -16,8 +16,6 @@ let parseWithTypeInfo (file, input) =
     | FSharpCheckFileAnswer.Succeeded(res) -> untypedRes, res
     | res -> failwithf "Parsing did not finish... (%A)" res
 
-
-
 let input = 
   """
 stdin.
@@ -29,17 +27,8 @@ System.IO.File.WriteAllText(file,input)
 
 let untyped, parsed = parseWithTypeInfo (file, input)
 
-
-let decls(keyword) = 
-    parsed.GetDeclarationListInfo(Some untyped, 1, 1, "", keyword, "", (fun () -> [])) 
-    |> Async.RunSynchronously
-
-let decls_stdin(keyword) = 
-    parsed.GetDeclarationListInfo(Some untyped, 2, 6, "", keyword, "", (fun () -> [])) 
-    |> Async.RunSynchronously
-
-let decls_stdout(keyword) = 
-    parsed.GetDeclarationListInfo(Some untyped, 3, 7, "", keyword, "", (fun () -> [])) 
+let decls(keyword, col, row) = 
+    parsed.GetDeclarationListInfo(Some untyped, col, row, "", keyword, "", (fun () -> [])) 
     |> Async.RunSynchronously
 
 type JsonFormat = { word : string; info: string list list  }
@@ -52,10 +41,14 @@ let extractGroupTexts = function
     | FSharpToolTipElement.CompositionError s      -> [s]
 
 let body =
-    [ []; ["System"] ; ["List"] ; ["Set"] ; ["Seq"] ; ["Array"] ; ["Map"] ; ["Option"] ; ["Observable"] ]
+    [ []; ["System"] ; ["List"] ; ["Set"] ; ["Seq"] ; ["Array"] ; ["Map"] ; ["Option"] ; ["Observable"] ; ["Microsoft";"FSharp";"Core";"Operators";"stdout"] ; ["Microsoft";"FSharp";"Core";"Operators";"stdin"] ]
     |> List.map ( fun l -> 
                     let label = if List.isEmpty l then "OneWord" else List.last l
-                    let body  = (decls l).Items
+                    let info  = match label with
+                                | "stdout" -> decls(l,3,7)
+                                | "stdin"  -> decls(l,2,6)
+                                | _        -> decls(l,1,1)
+                    let body  = info.Items
                                 |> Array.fold ( fun state x ->
                                     let dt : JsonFormat = { word = x.Name; info = match x.DescriptionText with FSharpToolTipText xs -> List.map extractGroupTexts xs }
                                     state + " + \"\\n\" + " + "\"\"\"" +  JsonConvert.SerializeObject ( dt ) + "\"\"\"" ) ""
@@ -63,34 +56,6 @@ let body =
                     String.replicate 12 " " + "\"" + label + "\"," + body.Substring(9)
                 )
     |> List.reduce ( fun a b -> a + "\n" + b )
-
-let body_stdin =
-    [["Microsoft";"FSharp";"Core";"Operators";"stdin"]]
-    |> List.map ( fun l -> 
-                    let label = if List.isEmpty l then "OneWord" else List.last l
-                    let body  = (decls_stdin l).Items
-                                |> Array.fold ( fun state x ->
-                                    let dt : JsonFormat = { word = x.Name; info = match x.DescriptionText with FSharpToolTipText xs -> List.map extractGroupTexts xs }
-                                    state + " + \"\\n\" + " + "\"\"\"" +  JsonConvert.SerializeObject ( dt ) + "\"\"\"" ) ""
-                                |> fun s -> s.Trim()
-                    String.replicate 12 " " + "\"" + label + "\"," + body.Substring(9)
-                )
-    |> List.reduce ( fun a b -> a + "\n" + b )
-
-let body_stdout =
-    [["Microsoft";"FSharp";"Core";"Operators";"stdout"]]
-    |> List.map ( fun l -> 
-                    let label = if List.isEmpty l then "OneWord" else List.last l
-                    let body  = (decls_stdout l).Items
-                                |> Array.fold ( fun state x ->
-                                    let dt : JsonFormat = { word = x.Name; info = match x.DescriptionText with FSharpToolTipText xs -> List.map extractGroupTexts xs }
-                                    state + " + \"\\n\" + " + "\"\"\"" +  JsonConvert.SerializeObject ( dt ) + "\"\"\"" ) ""
-                                |> fun s -> s.Trim()
-                    String.replicate 12 " " + "\"" + label + "\"," + body.Substring(9)
-                )
-    |> List.reduce ( fun a b -> a + "\n" + b )
-
-
 
 let head ="""
 namespace dummyJson
@@ -104,5 +69,4 @@ let foot ="""
         ]
 """
 
-head + body + "\n" + body_stdout + "\n" + body_stdin + foot |> stdout.WriteLine
-
+head + body + foot |> stdout.WriteLine
